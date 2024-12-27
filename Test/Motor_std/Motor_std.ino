45781f58a50965d8b1151f2fa76f9b524f7a6fc4
Timur Stepanov <tr12345678910s@gmail.com>
2024-12-27 18:48:38 +0300

    Update Motor_std.ino

--- Test/Motor_std/Motor_std.ino
+++ Test/Motor_std/Motor_std.ino
@@ -1,90 +1,62 @@
-#include <ESP8266WiFi.h>
-#include "GyverMotor.h"
+//#include <ESP8266WiFi.h>
+#include <GyverMotor.h> 
 
-class MotorSetup 
-{
-  public:
-    // Константы, пины, прочее
+class MotorSetup {
+public:
+  const int MOTOR_L1 = D0;
+  const int MOTOR_L2 = D7;
+  const int MOTOR_R1 = D5;
+  const int MOTOR_R2 = D6;
 };
 
 
-// Создаём объект сервера
-WiFiServer server(port);
-
 // Объявляем моторы
-const int MOTOR_L1 = D5;
-const int MOTOR_L2 = D6;
-const int MOTOR_R1 = D7;
-const int MOTOR_R2 = D8;
-int dir;
-
-MotorSetup motorSetup;
-GMotor motorL(DRIVER2WIRE, MOTOR_L1, MOTOR_L2, LOW);
-GMotor motorR(DRIVER2WIRE, MOTOR_R1, MOTOR_R2, LOW);
-
+MotorSetup MS;
+GMotor motorL(DRIVER2WIRE, MS.MOTOR_L1, MS.MOTOR_L2, LOW);
+GMotor motorR(DRIVER2WIRE, MS.MOTOR_R1, MS.MOTOR_R2, LOW);
 
 void setup() {
-  WiFi.persistent(false);
   Serial.begin(115200);
   delay(10);
-
-  // Подготавливаем светодиод (синий)
   pinMode(2, OUTPUT);
-  digitalWrite(2, 0);
-  /*pinMode(MOTOR_L1, OUTPUT);
-  pinMode(MOTOR_L2, OUTPUT);
-  pinMode(MOTOR_R1, OUTPUT);
-  pinMode(MOTOR_R2, OUTPUT);*/
-  
-  // Подготавливаем моторы
-  motorL.setMode(STOP);
-  motorL.setSpeed(255);
-  motorR.setMode(STOP);
-  motorL.setSpeed(255);
-
+}
 
 void loop() {
+  MoveForward();
+  digitalWrite(2, 0);
+  delay(3000);
 
-    MoveForward();
-    delay(2000);
-    MoveStop();
-    delay(1000);
-    MoveRight();
-    delay(3000);
-    MoveLight();
-    delay(2000);
-    MoveBack();
+  MoveRight();
+  digitalWrite(2, 1);
+  delay(3000);
 
-  }
+  MoveLeft();
+  digitalWrite(2, 0);
+  delay(3000);
+
+  MoveBack();
+  digitalWrite(2, 1);
+  delay(3000);
+}
 
 
-
-void MoveForward()
-{
+void MoveForward() {
   motorL.setMode(FORWARD);
   motorR.setMode(FORWARD);
 }
-
-void MoveStop()
-{
+void MoveStop() {
   motorL.setMode(STOP);
   motorR.setMode(STOP);
 }
-
-void MoveBack()
-{
+void MoveBack() {
   motorL.setMode(BACKWARD);
   motorR.setMode(BACKWARD);
 }
-
-void MoveLeft()
-{
+void MoveLeft() {
   motorL.setMode(BACKWARD);
   motorR.setMode(FORWARD);
 }
-
-void MoveRight()
-{
+void MoveRight() {
   motorL.setMode(FORWARD);
   motorR.setMode(BACKWARD);
 }
